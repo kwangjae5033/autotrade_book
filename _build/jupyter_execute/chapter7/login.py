31bc0d05a705edc31840777ff276697ffd99e0ad
kwangjae-novelis <kwangjae.kim@novelis.com>
2022-06-29 14:31:35 +0900

    accnt

--- _build/jupyter_execute/chapter7/login.py
+++ _build/jupyter_execute/chapter7/login.py
@@ -3,7 +3,7 @@
 
 # ## 자동 로그인
 
-# 서버 연결 및 로그인을 수행하려면 XASession 이라는 이름의 COM 클래스에 대한 객체를 선언하고 해당 객체가 제공하는 메서드를 호출해야 합니다. 아래는 서버연결 및 로그인을 차례대로 수행하는 코드입니다.
+# 서버연결 및 로그인을 수행하려면 XASession 이라는 이름의 COM 클래스에 대한 객체를 선언하고 해당 객체가 제공하는 메서드를 호출해야 합니다. 아래는 서버연결 및 로그인을 차례대로 수행하는 코드입니다.
 
 # In[ ]:
 
@@ -27,7 +27,7 @@ class Main:
             pythoncom.PumpWaitingMessages() 
 
 
-# 위 코드에서 MyObjects.server 와 MyObjects.login_ok 는 MyObjects 클래스에서 정의된 변수들입니다. 모의투자 서버에서 API 사용법을 익히기 위해 MyObjects.server 변수의 값은 'demo' 로 저장했으나, 실서버 사용을 원할 경우엔 'server' 로 저장하면 됩니다. 한편, 로그인 결과를 알기위해 MyObjects.login_ok 변수의 기본 값은 'False'로 저장하였고, 로그인 결과 메시지를 받으면 'True' 로 변경하여 위 코드의 while 문을 빠져 나옵니다.
+# 위 코드에서 MyObjects.server 와 MyObjects.login_ok 는 MyObjects 클래스에서 정의된 변수들입니다. 모의투자 서버에서 API 사용법을 익히기 위해 MyObjects.server 변수의 값은 'demo' 로 저장했으나, 실투자 서버를 사용을 원할 경우엔 'server' 로 저장하면 됩니다. 한편, 로그인 결과를 알기위해 MyObjects.login_ok 변수의 기본 값은 'False'로 저장하였고, 로그인 결과 메시지를 받으면 'True' 로 변경하여 위 코드의 while 문을 빠져 나옵니다.
 
 # 다음은 로그인 결과 메시지를 받기 위한 XS_event_handler 클래스 입니다. 로그인에 성공하면 증권서버는 szCode 변수에 "0000" 값을 반환합니다. 로그인에 성공하였으므로 MyObjects.login_ok 의 값을 'True' 로 변환 합니다. 반면, 로그인이 실패 시, 'False' 값을 유지 합니다.
 
@@ -45,13 +45,17 @@ class XS_event_handler:
             MyObjects.login_ok = False
 
 
-# 마지막으로 모의투자 서버 접속 시, 공동인증이 필요 없습니다. 아래 전체 코드를 싫행 시키고 로그인 결과를 확인 합니다. 
+# 마지막으로 모의투자 서버 접속 시, 공동인증이 필요 없습니다. 아래 전체 코드를 실행 시키고 로그인 결과를 확인 합니다. 
 
 # In[1]:
 
 
 import win32com.client #<
 import pythoncom #<
+
+'''
+로그인 하기
+'''
 
 # 앞으로 사용하게 될 변수들을 모아 놓는다.
 class MyObjects:
@@ -83,10 +87,4 @@ class Main:
 
 if __name__ == "__main__":
     Main()
-
-
-# In[ ]:
-
-
-
 
